13d1643ee6ce476d3bb6faafcd19f68f89a7d930
smithma <smith.ma@coupang.com>
2017-05-21 15:18:53 +0800

    build project structure

---
+++ src/main/java/com/mazhen/PaperCrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.mazhen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperCrawlerApplication.class, args);
+	}
+}
