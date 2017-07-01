4e2742a1521adb961fbe0cb6759d7475e321b190
smithma <smith.ma@coupang.com>
2017-07-02 07:35:30 +0800

    change execute method

--- src/main/java/com/mazhen/papercrawler/PaperCrawlerApplication.java
+++ src/main/java/com/mazhen/papercrawler/PaperCrawlerApplication.java
@@ -1,16 +1,32 @@
 package com.mazhen.papercrawler;
 
+import com.mazhen.papercrawler.service.ScheduledJobService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
 @SpringBootApplication
-@EnableScheduling
-public class PaperCrawlerApplication {
+//@EnableScheduling
+public class PaperCrawlerApplication implements CommandLineRunner {
+
+
+	@Autowired
+	private ScheduledJobService scheduledJobService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaperCrawlerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		if ("springer".equals(args[0])) {
+			scheduledJobService.executeSpringerSpider();
+		} else if ("cnki".equals(args[0])) {
+			scheduledJobService.executeCnkiSpider();
+		}
 	}
 }
